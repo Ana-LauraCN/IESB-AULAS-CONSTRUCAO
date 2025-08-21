7e79a353c0fa41c7a1b26fa9347784bbe171d23f
Ana-LauraCN <lauracastro.nas@gmail.com>
2025-08-20 21:29:51 -0300

    criando primeiro projeto

---
+++ Praticas/01-primeiro-projeto/index.js
@@ -0,0 +1,22 @@
+console.log("Rodando meu primeiro projeto em NODE")
+
+let nome = "ANA"
+
+let sobreNome = "Castro"
+
+console.log(nome + " " + sobreNome)
+
+let idade = 12
+
+console.log ("A idade do" + nome + "é:" + idade)
+
+
+let exerc = 0.5
+
+let trabalho = 1.5
+
+let prova = 4.5
+
+let notaA1cl = exerc + trabalho + prova
+
+console.log ("NOTA A1:" + notaA1cl)
